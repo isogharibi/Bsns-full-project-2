926b4fa3d79a0eb40df80601a9ef44bbc960dca9
mohammad <gharibi69068@gmail.com>
2023-07-19 02:31:17 -0700

    upload the file on the git hub

---
+++ css/script.js
@@ -0,0 +1,7 @@
+.con { width:180px; position:relative; border:1px solid #ccc; }
+.con input { margin:0 0 0 4px; border:0; }
+.con img { display:block; position:absolute; top:4px; left:164px; cursor:pointer; }​
+
+$(".con img").click(function() {
+  alert("Handler for .click() called.");
+});​
